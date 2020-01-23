ecc09cf2755f16bed26cf4ea9f57485b07ba4522
Scorpioeric-dev <ericscorpio1@gmail.com>
2020-01-23 08:30:11 -0700

    layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    list: [],
+    title: "",
+    input: "",
+    post: {}
+  };
+
+componentDidMount(){
+  this.handleAddTask()
+}
+
+  handleAddTask = ()=> {
+    this.setState({
+      list: [
+        ...this.state.list,
+        {
+          post: {
+            title: this.state.title,
+            content: this.state.input,
+            time: new Date().toLocaleTimeString([], {
+              timeStyle: "short",
+              dateStyle: "medium"
+            })
+          }
+        }
+      ],
+      input: "",
+      title: ""
+    });
+  }
+  
+  removePost= (name)=> {
+    this.setState({
+      list: this.state.list.filter(el => el !== name)
+    });
+  }
+
+
+
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
